e040881c96b925e19ffbef552e5bee3e80dd297a
leone <791960492@qq.com>
2024-10-18 09:14:42 +0000

    kafl fuzz

--- src/run/main.c
+++ src/run/main.c
@@ -8,8 +8,10 @@
 #include <sys/wait.h>
 #include <occlum_pal_api.h>
 #include <sys/prctl.h>
+#include "utils.h"
+#include <assert.h>
 
-int main(int argc, char *argv[]) {
+int _main(int argc, char *argv[]) {
     // Parse arguments
     if (argc < 2) {
         fprintf(stderr, "[ERROR] occlum-run: at least one argument must be provided\n\n");
@@ -36,9 +38,12 @@ int main(int argc, char *argv[]) {
     // Init Occlum PAL
     struct occlum_pal_attr attr = OCCLUM_PAL_ATTR_INITVAL;
     attr.log_level = getenv("OCCLUM_LOG_LEVEL");
+    hprintf("Before occlum_pal_init\n");
     if (occlum_pal_init(&attr) < 0) {
+        hprintf("occlum_pal_init fail\n");
         return EXIT_FAILURE;
     }
+    hprintf("occlum_pal_init success\n");
 
     // Use Occlum PAL to execute the cmd
     struct occlum_stdio_fds io_fds = {
@@ -49,14 +54,16 @@ int main(int argc, char *argv[]) {
     int exit_status = 0;
     int libos_tid = 0;
     struct occlum_pal_create_process_args create_process_args = {
-        .path = (const char *) cmd_path,
+        .path = (const char *) "/host/test_code",
         .argv = (const char **) cmd_args,
         .env = environ,
         .stdio = (const struct occlum_stdio_fds *) &io_fds,
         .pid = &libos_tid,
     };
+    hprintf("Before occlum_pal_create_process\n");
     if (occlum_pal_create_process(&create_process_args) < 0) {
         // Command not found or other internal errors
+        hprintf("occlum_pal_create_process fail\n");
         return 127;
     }
 
@@ -64,8 +71,10 @@ int main(int argc, char *argv[]) {
         .pid = libos_tid,
         .exit_value = &exit_status,
     };
+    hprintf("Before occlum_pal_exec\n");
     if (occlum_pal_exec(&exec_args) < 0) {
         // Command not found or other internal errors
+        hprintf("occlum_pal_exec fail\n");
         return 127;
     }
 
@@ -77,7 +86,30 @@ int main(int argc, char *argv[]) {
     }
 
     // Destroy Occlum PAL
+    hprintf("Before occlum_pal_destroy\n");
     occlum_pal_destroy();
 
     return exit_status;
+}
+
+int main(int argc, char **argv) {
+    agent_init(1);
+
+    // Register payload buffer
+    kAFL_payload *pbuf =
+        (kAFL_payload *)malloc_resident_pages(PAYLOAD_MAX_SIZE / PAGE_SIZE);
+    assert(pbuf);
+    kAFL_hypercall(HYPERCALL_KAFL_GET_PAYLOAD, (uint64_t)pbuf);
+    hrange_submit(0, 0x1000, 0x7fffffffffff);
+
+    while (1) {
+        kAFL_hypercall(HYPERCALL_KAFL_USER_FAST_ACQUIRE, 0);
+        hprintf("Data %p %d\n", pbuf->data, pbuf->size);
+        write_to_file("./test_code", pbuf->data, pbuf->size);
+        hprintf("Before main\n");
+        _main(argc, argv);
+        hprintf("After main\n");
+        kAFL_hypercall(HYPERCALL_KAFL_RELEASE, 0);
+        hprintf("Round finish\n");
+    }
 }
